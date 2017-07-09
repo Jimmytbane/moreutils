864183bf6a6e0ac2939e1a41bfc513f3e63f8f8b
JimmyBot <jimmybot@teknik.io>
2017-07-09 10:31:39 -0400

    replace c with go in this case

--- src/entropy.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main()
-{
-  printf("Linux entropy --> ");
-  fflush(stdout);
-  system("cat /proc/sys/kernel/random/entropy_avail");
-}
--- src/entropy.go
+++ src/entropy.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-  "os/exec"
+	"fmt"
+	"os/exec"
 )
 
-func main() { // not sure why this function isn't working right now, I'll fix it later.
-  exec.Command("bin/sh", "-c", "go", "version").Output()
+func main() {
+  out, err := exec.Command("cat", "/proc/sys/kernel/random/entropy_avail").Output()
+	if err != nil {
+		fmt.Println("Error1")
+	}
+  fmt.Printf("Linux entropy --> %s", out)
 }
