ff13987b660a2a6e5d6120e23dd28f26a7e2c5a2
JimmyBot <jimmybot@teknik.io>
2017-08-07 13:11:31 -0400

    formatting

--- src/ntkernelbestkernel.rb
+++ src/ntkernelbestkernel.rb
@@ -1,3 +1,10 @@
+#!/usr/bin/env ruby
+
 puts "What is the best kernel of all time"
-puts "Input your reponse"
-input1 = gets.chomp
+printf "Input your reponse:  "
+input1 = gets.chomp.downcase
+	if input1 == "NT"
+		puts "NT KERNEL BEST KERNEL"
+	else
+		puts "What are you talking about? NT KERNEL BEST KERNEL" 
+end
